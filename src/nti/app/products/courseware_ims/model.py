b4e870c11f352229af1c92551cb5835c33ff6671
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-09-06 02:28:38 +0000

    ext:registerAutoPackageIO
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_ims/trunk@72480 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_ims/model.py
+++ src/nti/app/products/courseware_ims/model.py
@@ -15,34 +15,18 @@ from zope.schema.fieldproperty import FieldPropertyStoredThroughField as FP
 
 from nti.app.products.courseware.enrollment import EnrollmentOption
 
-from nti.common.persistence import NoPickle
 from nti.common.representation import WithRepr
-
-from nti.externalization.interfaces import LocatedExternalDict
-from nti.externalization.interfaces import StandardExternalFields
-from nti.externalization.interfaces import IInternalObjectExternalizer
 
 from nti.schema.schema import EqHash
 
 from .interfaces import IIMSEnrollmentOption
 
-CLASS = StandardExternalFields.CLASS
-MIMETYPE = StandardExternalFields.MIMETYPE
-
 @WithRepr
-@NoPickle
 @EqHash('SourcedID',)
-@interface.implementer(IIMSEnrollmentOption, IInternalObjectExternalizer)
+@interface.implementer(IIMSEnrollmentOption)
 class IMSEnrollmentOption(EnrollmentOption):
 
 	__external_class_name__ = "IMSEnrollment"
 	mime_type = mimeType = 'application/vnd.nextthought.courseware.imsenrollmentoption'
 
 	SourcedID = FP(IIMSEnrollmentOption['SourcedID'])
-
-	def toExternalObject(self, *args, **kwargs):
-		result = LocatedExternalDict()
-		result[MIMETYPE] = self.mimeType
-		result[CLASS] = self.__external_class_name__
-		result['SourcedID'] = result['sourcedid'] = self.SourcedID
-		return result
