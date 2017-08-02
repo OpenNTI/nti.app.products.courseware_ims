c74d5789d9fb5347670c16aef03e70aeeda8b830
Sheldon Smith <sheldon.smith@nextthought.com>
2017-08-02 09:24:32 -0500

    Added traversable path

--- src/nti/app/products/courseware_ims/lti.py
+++ src/nti/app/products/courseware_ims/lti.py
@@ -11,7 +11,19 @@ from zope.annotation import IAnnotations
 
 from zope import interface
 
+from zope.container.contained import Contained
+
+from zope.traversing.interfaces import IPathAdapter
+
 from nti.app.products.courseware_ims.interfaces import ICourseConfiguredToolContainer
+
+from nti.dataserver import authorization as nauth
+
+from nti.dataserver.authorization_acl import ace_allowing
+from nti.dataserver.authorization_acl import acl_from_aces
+
+from nti.dataserver.interfaces import ACE_DENY_ALL
+
 
 from nti.ims.lti.consumer import ConfiguredToolContainer
 
@@ -31,3 +43,19 @@ def course_to_configured_tool_container(course, create=True):
         tools.__parent__ = course
         annotations[TOOLS_ANNOTATION_KEY] = tools
     return tools
+
+
+@interface.implementer(IPathAdapter)
+class LTIConfiguredToolsAdapter(Contained):
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.__parent__ = context
+
+    # TODO update to correct privileges
+    def __acl__(self):
+        aces = [ace_allowing(nauth.ROLE_ADMIN, type(self)),
+                ACE_DENY_ALL]
+        acl = acl_from_aces(aces)
+        return acl
