a398f879f44d906e4e9bffad98d2b2dacf7a87ce
Sheldon Smith <sheldon.smith@nextthought.com>
2017-10-12 18:44:44 -0400

    Pep8

--- src/nti/app/products/courseware_ims/views.py
+++ src/nti/app/products/courseware_ims/views.py
@@ -35,6 +35,9 @@ from nti.app.externalization.error import raise_json_error
 
 from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtilsMixin
 
+from nti.app.products.courseware_ims.interfaces import IExternalToolAsset
+from nti.app.products.courseware_ims.interfaces import ILTILaunchParamBuilder
+
 from nti.app.products.courseware_ims.lti import LTIExternalToolAsset
 
 from nti.app.products.courseware_ims.interfaces import IExternalToolAsset
