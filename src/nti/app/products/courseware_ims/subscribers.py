780d2b0e13a699b3efb0cdd021ea0ed727d456ad
Sheldon Smith <sheldon.smith@nextthought.com>
2017-10-17 18:39:49 -0400

    Add LTIInstanceParams

--- src/nti/app/products/courseware_ims/subscribers.py
+++ src/nti/app/products/courseware_ims/subscribers.py
@@ -3,8 +3,6 @@
 
 from __future__ import print_function, absolute_import, division
 
-from nti.dataserver.users import User
-
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -13,34 +11,43 @@ from zope import interface
 
 from nti.app.products.courseware_ims.interfaces import ILTILaunchParamBuilder
 
+from nti.dataserver.users import User
+
 from nti.externalization.oids import toExternalOID
 
 
-@interface.implementer(ILTILaunchParamBuilder)
-class LTIResourceParams(object):
+class LTIParams(object):
 
-    def __init__(self, request):
+    def __init__(self, request, context):
         self.request = request
+        self.context = context
+
+
+@interface.implementer(ILTILaunchParamBuilder)
+class LTIResourceParams(LTIParams):
 
     def build_params(self, params):
-        # This is makes some assumptions about the request that may not be ideal
-        asset = self.request.context
+        asset = self.context
         asset_oid = toExternalOID(asset)
         params['resource_link_id'] = asset_oid
         params['resource_link_title'] = asset.ConfiguredTool.config.title
-        params['resource_link_description'] = asset.ConfiguredTool.config.title
+        params['resource_link_description'] = asset.ConfiguredTool.config.description
 
 
 @interface.implementer(ILTILaunchParamBuilder)
-class LTIUserParams(object):
-
-    def __init__(self, request):
-        self.request = request
+class LTIUserParams(LTIParams):
 
     def build_params(self, params):
         user_obj = User.get_user(self.request.authenticated_userid)
         params['user_id'] = toExternalOID(user_obj)
-        roles = []
-        for role in user_obj.dynamic_memberships:
-            roles.append(role.__name__)
-        params['roles'] = roles
+        params['roles'] = [role.__name__ for role in user_obj.dynamic_memberships]
+        # TODO find user attributes on user_obj for lis_person_* params
+
+
+@interface.implementer(ILTILaunchParamBuilder)
+class LTIInstanceParams(LTIParams):
+
+    def build_params(self, params):
+        params['tool_consumer_instance_guid'] = self.request.domain
+        params['tool_consumer_instance_name'] = "Placeholder"  # TODO find where this is on the request
+        params['tool_consumer_instance_url'] = self.request.host_url
