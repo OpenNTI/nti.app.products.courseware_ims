642ecf3b6e1c45660634ef53a4c3a0499562bf80
Sheldon Smith <sheldon.smith@nextthought.com>
2018-07-09 13:33:38 -0500

    Grab external ntiid for Configured Tools and patch test

--- src/nti/app/products/courseware_ims/externalization.py
+++ src/nti/app/products/courseware_ims/externalization.py
@@ -16,6 +16,8 @@ from nti.externalization.externalization import to_standard_external_dictionary
 from nti.externalization.interfaces import IExternalObject
 from nti.externalization.interfaces import StandardExternalFields
 
+from nti.ntiids.oids import to_external_ntiid_oid
+
 ITEMS = StandardExternalFields.ITEMS
 
 __docformat__ = "restructuredtext en"
@@ -33,6 +35,6 @@ class _CourseConfiguredToolContainerExternalObject(object):
     def toExternalObject(self, **kwargs):
         result = to_standard_external_dictionary(self.container, **kwargs)
         result[ITEMS] = {
-            key: to_external_object(val, name='exporter') for key, val in self.container.items()
+            to_external_ntiid_oid(val): to_external_object(val, name='exporter') for val in self.container.values()
         }
         return result
