073e8c59a98f7dd89fd6d951c3cfae37a0a70fdc
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-07-18 13:31:43 -0500

    sort imports

--- src/nti/app/products/courseware_ims/lti.py
+++ src/nti/app/products/courseware_ims/lti.py
@@ -8,9 +8,9 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-import six
-
 from persistent import Persistent
+
+import six
 
 from zope import component
 from zope import interface
