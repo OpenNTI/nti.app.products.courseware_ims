d035480f9bbf75b263358ba65253470dbafe6d36
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-07-18 13:23:44 -0500

    headers; pylint

--- src/nti/app/products/courseware_ims/datastructures.py
+++ src/nti/app/products/courseware_ims/datastructures.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
 
-from __future__ import print_function, absolute_import, division
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from nti.contentfile.datastructures import BaseFactory
 
 from nti.ims.lti.consumer import ConfiguredTool
-
-__docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
