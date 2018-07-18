f06ede31b217b56cd1ecc3ce79444e52d9542753
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-07-18 13:24:39 -0500

    pep8

--- src/nti/app/products/courseware_ims/decorators.py
+++ src/nti/app/products/courseware_ims/decorators.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
 
 from __future__ import division
 from __future__ import print_function
@@ -25,10 +28,9 @@ from nti.links import Link
 
 LINKS = StandardExternalFields.LINKS
 
-logger = __import__('logging').getLogger(__name__)
-
-
 LAUNCH_PATH = '@@launch'
+
+logger = __import__('logging').getLogger(__name__)
 
 
 @component.adapter(ICourseInstance)
@@ -48,4 +50,6 @@ class _ExternalToolAssetInstanceLinkDecorator(Singleton):
 
     def decorateExternalMapping(self, context, result):
         _links = result.setdefault(LINKS, [])
-        _links.append(Link(context, rel=EXTERNAL_TOOL_ASSET_LAUNCH, elements=(LAUNCH_PATH,)))
+        _links.append(
+            Link(context, rel=EXTERNAL_TOOL_ASSET_LAUNCH, elements=(LAUNCH_PATH,))
+        )
