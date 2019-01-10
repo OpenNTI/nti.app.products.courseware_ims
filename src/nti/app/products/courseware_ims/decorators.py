96ad395f237c9e4cd90eb2cbf1fe53959fa0e78a
Sheldon Smith <sheldon.smith@nextthought.com>
2019-01-10 14:50:51 -0600

    Patch progress for External Tool Assets in section courses

--- src/nti/app/products/courseware_ims/decorators.py
+++ src/nti/app/products/courseware_ims/decorators.py
@@ -8,10 +8,13 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+from pyramid.threadlocal import get_current_request
+
 from zope import component
 from zope import interface
 
 from nti.app.products.courseware_ims import LTI_CONFIGURED_TOOLS
+from nti.app.products.courseware_ims import LTI_EXTERNAL_TOOL_ASSETS
 from nti.app.products.courseware_ims import EXTERNAL_TOOL_ASSET_LAUNCH
 
 from nti.app.products.courseware_ims.interfaces import ICourseConfiguredToolContainer
@@ -25,6 +28,8 @@ from nti.externalization.interfaces import StandardExternalFields
 from nti.externalization.singleton import Singleton
 
 from nti.links import Link
+
+from nti.ntiids.ntiids import find_object_with_ntiid
 
 LINKS = StandardExternalFields.LINKS
 
@@ -49,7 +54,27 @@ class _CourseInstanceLinkDecorator(Singleton):
 class _ExternalToolAssetInstanceLinkDecorator(Singleton):
 
     def decorateExternalMapping(self, context, result):
+        request = get_current_request()
+        course = ICourseInstance(request, None)
+        # XXX: We should always have a course here, but because of some of the handshaking
+        # required for the tool launch there are cases where the web app GETs the asset again
+        # midway through the process. The course is passed in the query params in these cases
+        if course is None:
+            # see if the course is in the request params
+            course_ntiid = request.params.get('course', None)
+            course = find_object_with_ntiid(course_ntiid)
         _links = result.setdefault(LINKS, [])
-        _links.append(
-            Link(context, rel=EXTERNAL_TOOL_ASSET_LAUNCH, elements=(LAUNCH_PATH,))
-        )
+        # If we can find a course, we want to decorate a link that allows for traversal
+        if course is not None:
+            context_ntiid = context.ntiid
+            _links.append(
+                Link(course,
+                     rel=EXTERNAL_TOOL_ASSET_LAUNCH,
+                     elements=(LTI_EXTERNAL_TOOL_ASSETS, context_ntiid, LAUNCH_PATH))
+            )
+        # This is less than ideal but we will directly resolve here
+        else:
+            logger.info(u'Unable to locate a course for External Tool Asset link decoration')
+            _links.append(
+                Link(context, rel=EXTERNAL_TOOL_ASSET_LAUNCH, elements=(LAUNCH_PATH,))
+            )
